3f9c8d058eca696d3120b17c0d0f9e6a7ccd376a
Keven Nogueira <kevennogueira161@gmail.com>
2023-04-08 14:38:55 -0300

    first commit

---
+++ consulta_chamado.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+
+    <!-- Fontewesom -->
+    <script src="https://kit.fontawesome.com/7d3a8355c9.js" crossorigin="anonymous"></script>
+
+    <!-- CSS Interno -->
+    <link rel="stylesheet" href="CSS/style.css" />
+
+    <title>App Help Desk</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">
+            <img src="IMG/logo.png" width="30" height="30" alt="Logo Help Desck" class="d-inline-block align-top">
+            App Help Desk
+        </a>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="card-consultar-chamado">
+                <div class="card">
+                    <div class="card-header">
+                        Consulta de chamado
+                    </div>
+                    <div class="card-body">
+                        <div class="card mb-3 bg-light">
+                            <div class="card-body">
+                                <h5 class="card-title">Título do chamado...</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
+                                <p class="card-text">Descrição do chamado...</p>
+                            </div>
+                        </div>
+                        <div class="card mb-3 bg-light">
+                            <div class="card-body">
+                                <h5 class="card-title">Título do chamado...</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
+                                <p class="card-text">Descrição do chamado...</p>
+                            </div>
+                        </div>
+                        <div class="row mt-5">
+                            <div class="col-6">
+                                <button class="btn btn-lg btn-warning btn-block" type="submit">Voltar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
+
+    <!-- Incio do JS personalizado -->
+    <script src="JS/script.js"></script>
+</body>
+
+</html>
