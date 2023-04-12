37294612eb009ec62e6f29f3ac15c244fd71b374
Keven Nogueira <kevennogueira161@gmail.com>
2023-04-12 16:47:52 -0300

    Inico da criação da logica para cadastro de novos usuarios por tela

--- cadastra_usuario.php
+++ cadastra_usuario.php
@@ -5,7 +5,12 @@ print_r($_POST);
 echo '</pre>';
 
 if ($_POST['senha'] != $_POST['senha2']) {
+
     header('Location: criacao_usuario.php?senha=erro');
 } else {
+
+
+
+
     header('Location: home.php?usuario=sucesso');
-}
+};
