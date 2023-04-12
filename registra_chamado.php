380f745f3a86d8ae357318d3095a189abddeb11f
Keven Nogueira <kevennogueira161@gmail.com>
2023-04-12 15:19:24 -0300

    Adição de logica para bloquei de abertura de chamados com os campos vazios!

--- registra_chamado.php
+++ registra_chamado.php
@@ -4,11 +4,16 @@ session_start();
 $titulo = str_replace('|', '#', $_POST['titulo']);
 $descricao = str_replace('|', '#', $_POST['descricao']);
 
-$chamado = $_SESSION['id'] . '|' . $titulo . '|' . $_POST['categoria'] . '|' . $descricao . PHP_EOL;
+if ($titulo == '' || $descricao == '') {
+    header('Location: abrir_chamado.php?chamado=erro');
+} else {
 
-$arquivo_chamado = fopen('D:\Downloads\Programacao\XAMPP\XAMPP\htdocs\WorkSpace\PHP Basico\App Help Desk\Arquivo_Chamado\arquivo.txt', 'a');
+    $chamado = $_SESSION['id'] . '|' . $titulo . '|' . $_POST['categoria'] . '|' . $descricao . PHP_EOL;
 
-fwrite($arquivo_chamado, $chamado);
+    $arquivo_chamado = fopen('D:\Downloads\Programacao\XAMPP\XAMPP\htdocs\WorkSpace\PHP Basico\App Help Desk\Arquivo_Chamado\arquivo.txt', 'a');
 
-fclose($arquivo_chamado);
-header('Location: abrir_chamado.php?chamado=sucesso');
+    fwrite($arquivo_chamado, $chamado);
+
+    fclose($arquivo_chamado);
+    header('Location: abrir_chamado.php?chamado=sucesso');
+}
